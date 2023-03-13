7982dbbdacf7a3653d8426f2464f3d1dfde640e2
karly22 <carlita.lmj15@gmail.com>
2023-03-12 23:11:14 -0400

    constantes

---
+++ assets/js/principal.js
@@ -0,0 +1,9 @@
+//declarando constantes
+const KEY_UP = 38;//arriba
+const KEY_DOWN = 40;//abajo
+const KEY_RIGHT = 39;//derecha
+const KEY_LEFT = 37;//izquierda
+const KEY_SPACE = 32;//espacio
+
+const GAME_WIDTH = 800;//ancho
+const GAME_HEIGHT = 600;//alto
