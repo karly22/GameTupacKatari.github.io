80e523a21acdab45b23ffd2dd59526df2eca1476
karly22 <carlita.lmj15@gmail.com>
2023-03-12 23:14:55 -0400

    const-1

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -7,3 +7,21 @@ const KEY_SPACE = 32;//espacio
 
 const GAME_WIDTH = 800;//ancho
 const GAME_HEIGHT = 600;//alto
+
+const STATE = {
+    x_pos : 0,//posiciones
+    y_pos : 0,//posiciones
+    move_right: false,
+    move_left: false,
+    shoot: false,//disparos
+    lasers: [],//balas
+    enemyLasers: [],//disparos de los enemigos
+    enemies : [],//
+    spaceship_width: 50,//nave espacial
+    enemy_width: 50,//ancho del enemigo
+    cooldown : 0,//enfriamiento
+    number_of_enemies: 16,//numero de enemigos
+    enemy_cooldown : 0,// enfriamiento
+    gameOver: false//fin del juego
+  }
+  
